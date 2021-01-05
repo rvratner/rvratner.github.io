86f31941cff945080f852cb262cc25a3398ed474
Rachel Ratner <rachelra@vulcan.com>
2021-01-05 15:12:45 -0800

    updates

--- js/script.js
+++ js/script.js
@@ -1,2 +1,20 @@
 console.log('hello!')
-// insert fun game here
+
+
+let gradient = true;
+
+const changeBackgroundColor = () => {
+	if (!gradient) document.querySelector('.page').classList.add('gradient');
+	else document.querySelector('.page').classList.remove('gradient');
+};
+
+const randomStuff = () => {
+	gradient = !gradient;
+	changeBackgroundColor(gradient);
+	// happened today in history
+	// change color
+	// disco
+	// translate to spanish
+};
+
+document.getElementById('clickme').addEventListener('click', randomStuff);
